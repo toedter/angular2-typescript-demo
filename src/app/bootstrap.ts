04a6ad5907e0ed8ec30e207f573e33b1d0292e98
Kai Toedter <kai.toedter@siemens.com>
2015-10-31 15:49:43 +0100

    smaller bootstrap

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,7 +1,6 @@
 import {bootstrap, provide} from 'angular2/angular2';
 import {ROUTER_PROVIDERS, Location, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {AppComponent} from './app/AppComponent';
-import {MoviesComponent} from './movies/MoviesComponent';
 import {MovieService} from './movies/MovieService';
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS, MoviesComponent, MovieService, provide(LocationStrategy, {useClass: HashLocationStrategy})]);
+bootstrap(AppComponent, [MovieService, ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})]);
